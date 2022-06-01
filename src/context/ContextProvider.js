72f44a67a3d7265f815103062ddc533400fe7362
Zurcemozz <zurcemozz@gmail.com>
2022-06-01 13:24:45 +0800

    navbar section complete

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -10,9 +10,26 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true)
+  const [isClicked, setIsClicked] = useState(initialState)
+
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true })
+  }
+
+  const [screenSize, setscreenSize] = useState(undefined)
 
   return (
-    <StateContext.Provider value={{ activeMenu, setActiveMenu }}>
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setscreenSize,
+      }}
+    >
       {children}
     </StateContext.Provider>
   )
